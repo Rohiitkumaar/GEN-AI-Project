9e97b1184a1f4bbe5a4c92f115e0e937aabbece0
Rohiitkumaar <rohitkumarverma289@gmail.com>
2026-07-28 10:28:55 +0530

    Frontend UI part is done

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router"
 import Login from "./features/auth/pages/Login.jsx"
 import Register from "./features/auth/pages/Register.jsx";
 import Protected from "./features/auth/components/Protected.jsx";
+import Home from "./features/interview/pages/Home.jsx";
+import Interview from "./features/interview/pages/Interview.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -15,9 +17,14 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <Protected>
-        <h1>Home Page</h1>
-      </Protected>
+      // <Protected>
+      //   <Home/>
+      // </Protected>
+      <Home />
     ),
   },
+  {
+    path: "/interview/:interviewId",
+    element: <Interview/>
+  }
 ]);
