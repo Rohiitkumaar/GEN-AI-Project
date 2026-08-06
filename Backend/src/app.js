ab8e8d26b0710a5a9b87ee578c3df67ef16e5927
Rohiitkumaar <rohitkumarverma289@gmail.com>
2026-08-06 11:39:26 +0530

    Frontend URL added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:5173", "https://jobfit-ai.rohitverma.tech"],
     credentials: true,
   }),
 );
